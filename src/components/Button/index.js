86de4e3bf5b674cff94a8e22f4530d66c87ee38c
maykovski <maykovski@ecredit.one>
2021-01-28 23:12:59 +0300

    #188 add prop-types in Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import stylePropType from 'react-style-proptype';
 import cn from 'classnames';
 import './Button.css';
 
@@ -6,7 +8,7 @@ const Button = ({
   tag: Tag = 'button',
   style,
   onPress = () => {},
-  icon,
+  icon = '',
   children,
   classList = [],
   className = '',
@@ -23,6 +25,16 @@ const Button = ({
       {children && <div className="welcomeview__text">{children}</div>}
     </Tag>
   );
+};
+
+Button.propTypes = {
+  tag: PropTypes.string,
+  style: stylePropType,
+  onPress: PropTypes.func,
+  icon: PropTypes.string,
+  children: PropTypes.string,
+  classList: PropTypes.arrayOf(PropTypes.string),
+  className: PropTypes.string,
 };
 
 export default Button;
